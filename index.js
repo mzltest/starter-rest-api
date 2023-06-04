ab9d13cc2474e9615a5cf86720dd436990db10a2
mzltest <52124882+mzltest@users.noreply.github.com>
2023-06-04 05:06:18 +0000

    sa

--- index.js
+++ index.js
@@ -110,8 +110,8 @@ app.get('/api/verify/:id/:answer', async (req, res) => {
   answer=req.params.answer
   answer=answer.split('|')
   answer=answer.sort()
-  chal=await db.collection('challenges').get(cid).props
-  console.log(chal)
+  chal=await db.collection('challenges').get(cid)
+  console.log(answer)
   if (!chal){
     res.json({ok:false,err:'no such challenge',reload:true}).end()
     return
