9264e6516d2ca1f2c49bb6a1016f54c44e4bbcbb
mzltest <52124882+mzltest@users.noreply.github.com>
2023-06-04 04:52:46 +0000

    iw

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.get('/api/create', async (req, res) => {
   console.log(pendimgs)
   for (im of pendimgs){
     
-    tempim=await sharp(`./cpb/${im}`).blur(Math.random()*20+0.3).resize({ width: 256 }).toBuffer()
+    tempim=await sharp(`./cpb/${im}`).blur(getRandomInt(1,10)).resize({ width: 256 }).toBuffer()
     nid=await nanoid.nanoid(6)
     imgs.push({'base64':tempim.toString('base64'),id:nid})
 
