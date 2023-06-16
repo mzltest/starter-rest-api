996c509df4bbf44337511f65a27ce832970e0fe7
mzltest <52124882+mzltest@users.noreply.github.com>
2023-06-16 16:39:45 +0000

    ohohohoh

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ usepng=req.body.png
 filename=await nanoid.nanoid(6)+(usepng?'.png':'.jpg')
 size=req.body.size
 try{
+console.log(exec(`rm -rf /tmp/*`).toString("utf8"))
 console.log(exec('cp bin/* /tmp/').toString("utf8"))
 console.log(exec('mkdir /tmp/.fonts').toString("utf8"))
 console.log(exec('cp /tmp/NotoSans-Regular.ttf /tmp/.fonts/').toString("utf8"))
@@ -74,7 +75,7 @@ if (!url){
 }
 console.log(exec(`bin/phantomjs bin/rasterize.js "${url}" /tmp/${filename} ${size}`).toString("utf8"))
 res.sendFile(`/tmp/${filename}`)
-console.log(exec(`ls /tmp/* -l`).toString("utf8"))
+
 })
 
 // a b c in [0,3)
